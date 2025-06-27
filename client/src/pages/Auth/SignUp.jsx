c962be93f54038a54a2862e95734e38df3ea6889
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-27 23:40:45 +0300

    build signup ui 50%

--- client/src/pages/Auth/SignUp.jsx
+++ client/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
 
 const SignUp = () => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  // handle signup form submit
+  const handleSignup = async (e) => {};
   return (
-    <div>SignUp</div>
-  )
-}
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.
+        </p>
+        <form onSubmit={handleSignup}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John Deo"
+              type="text"
+            />
+          </div>
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
 
-export default SignUp
+export default SignUp;
