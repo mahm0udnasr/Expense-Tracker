6ebb51e64cbd7b84b4f1e6054140afdff7d56183
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-07-04 07:59:58 +0300

    add axios instance

---
+++ client/src/utils/axiosInstance.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { BASE_URL } from "./api";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// response interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401) {
+        window.location.href = "/login";
+      } else if (error.response.status === 500) {
+        console.error("Server error, please try again later.");
+      }
+    } else if (error.code == "ECONNABORTED") {
+      console.error("Request timed out, please check your internet connection.");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
