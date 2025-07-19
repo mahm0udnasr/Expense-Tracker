11dc310063d694ee30784359a1bf9a7d8585792c
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-07-19 07:29:32 +0300

    add new helper functions

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export const validateEmail = (email) => {
   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return regex.test(email);
@@ -18,6 +20,18 @@ export const prepareExpenseBarCharData = (data = []) => {
   const charData = data.map((item) => ({
     category: item?.category,
     amount: item?.amount,
+  }));
+  return charData;
+};
+
+export const prepareIncomeBarCharData = (data = []) => {
+  const sortedData = [...data].sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+  const charData = sortedData.map((item) => ({
+    month: moment(item?.date).format("Do MMM"),
+    amount: item?.amount,
+    source: item?.source,
   }));
   return charData;
 };
